2e12086a5d6ef936dc4c336fe626853b20d12c95
ollyroberts <olly.roberts001@gmail.com>
2018-04-13 14:02:53 +0100

    moddified cgi location

---
+++ cgi-bin/cgi_sample.py
@@ -0,0 +1,11 @@
+#C:\\Users\\Antony\\AppData\\Local\\Programs\\Python\\Python36-32
+
+
+import cgi
+cgi.test()
+
+""" apparently I have to cmod it in bash 
+
+bash> chmod a+x cgi-bin/hello.py
+
+"""
